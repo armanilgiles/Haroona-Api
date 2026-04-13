eb367a2441258d3a5bba0f680fc71e94b0e77638
ArmaniGiles <armanigiles@outlook.com>
2026-04-13 16:07:19 -0400

    Allow Vercel preview origins for api dev

--- app/main.py
+++ app/main.py
@@ -33,9 +33,10 @@ app.add_middleware(
         "https://www.aruona.com",
         "https://haroona-web.vercel.app",  # prod frontend (vercel)
         "https://api-dev.haroona.com",
-        "https://haroona-web-git-main-bless727-9934s-projects.vercel.app",
 
     ],
+        allow_origin_regex=r"^https://haroona.*-bless727-9934s-projects\.vercel\.app$",
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
